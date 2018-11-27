0bbf82762c56b898f26f8d671e2230f3bf5c0218
LEI Sen <Listen180@gmail.com>
2018-11-27 16:57:06 +0800

    bug fixed: float number forced

--- dplearn/tools.py
+++ dplearn/tools.py
@@ -152,9 +152,9 @@ def cord_distance(Lng_A, Lat_A, Lng_B, Lat_B):
         pA = atan(rb / ra * tan(rad_lat_A))
         pB = atan(rb / ra * tan(rad_lat_B))
         xx = acos(sin(pA) * sin(pB) + cos(pA) * cos(pB) * cos(rad_lng_A - rad_lng_B))
-        c1 = (sin(xx) - xx) * (sin(pA) + sin(pB)) ** 2 / cos(xx / 2) ** 2
-        c2 = (sin(xx) + xx) * (sin(pA) - sin(pB)) ** 2 / sin(xx / 2) ** 2
-        dr = flatten / 8 * (c1 - c2)
+        c1 = (sin(xx) - xx) * (sin(pA) + sin(pB)) ** 2.0 / cos(xx / 2.0) ** 2.0
+        c2 = (sin(xx) + xx) * (sin(pA) - sin(pB)) ** 2.0 / sin(xx / 2.0) ** 2.0
+        dr = flatten / 8.0 * (c1 - c2)
         distance = ra * (xx + dr)
     return distance
 
