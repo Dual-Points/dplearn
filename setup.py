9c5474404679a4c1da456a010138608c45567441
LEI Sen <Listen180@gmail.com>
2018-11-28 22:10:16 +0800

    update on setup.py: use README.rst instead

--- setup.py
+++ setup.py
@@ -5,18 +5,26 @@
 # * Author        : LEI Sen
 # * Email         : sen.lei@outlook.com
 # * Create time   : 2018-11-21 15:48
-# * Last modified : 2018-11-21 21:41
+# * Last modified : 2018-11-28 22:09
 # * Filename      : setup.py
 # * Description   : 
 # *********************************************************
 
 import setuptools
 import sys
+import codecs
+
 
 version_number = input("Input the new version number you are going to use: ")
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+# Get the long description
+with codecs.open('README.rst') as f:
+    long_description = '\n{}'.format(f.read())
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
 
 setuptools.setup(
     name="dplearn",
@@ -31,7 +39,7 @@ setuptools.setup(
     license="BSD 3-clause",
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         #"License :: OSI Approved :: MIT License",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
